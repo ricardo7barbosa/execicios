a55210bdefd51dc309ebfd501ee2b1193d56378e
Ricardo Barbosa da Silva <ricardosevenotion@gmail.com>
2024-04-26 16:50:29 -0300

    Atualizando o código

--- media.py
+++ media.py
@@ -3,4 +3,4 @@
 nota1 = float(input("Digite a primeira nota:"))
 nota2 = float(input("Digite a segunda nota:"))
 final = (nota1+nota2)/2
-print("A média semestral é:", final)
+print("A média semestral é:", round (final,2))
