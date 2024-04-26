bd575d72a1fdeb30f6ef0298275c1634a94820f5
ricardo7barbosa <ricardosevenotion@gmail.com>
2024-04-26 12:08:57 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,6 @@
+#Programa média semestral de aluno
+
+nota1 = float(input("Digite a primeira nota"))
+nota2 = float(input("Digite a segunda nota"))
+final = nota1/nota2
+print("A média semestral é," final)
