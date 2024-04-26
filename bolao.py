f61f8ad4162c2ebc42a55d09ee3fae25967ddedc
ricardo7barbosa <ricardosevenotion@gmail.com>
2024-04-26 12:24:04 -0300

    Create bolao.py

---
+++ bolao.py
@@ -0,0 +1,7 @@
+#Programa Bolão
+
+premio = float(input("Digite o valor do prêmio:"))
+partic = float(input("Digite o número de participantes:"))
+div = premio/partic
+print("Cada participante receberá:", round(div,2))
+               
